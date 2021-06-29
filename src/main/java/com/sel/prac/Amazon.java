666c28588760422261f27ea6961943b816c99069
Ritu <ritu.ridam1@gmail.com>
2021-06-29 09:30:59 -0500

    amazon page file browser

--- src/main/java/com/sel/prac/Amazon.java
+++ src/main/java/com/sel/prac/Amazon.java
@@ -1,14 +1,21 @@
 package com.sel.prac;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Amazon {
 	WebDriver driver;
 	
 	@Test
 	public void start() {
-		
+		WebDriverManager.chromedriver().setup();
+		driver.navigate().to("http://www.amazon.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		
 		
 	}
